ca9336a6921df9b263330083ba4da717bdf1749d
davizf <100363798@alumnos.uc3m.es>
2019-12-10 16:07:49 +0100

    completed bank

--- MicroserviceBank/src/main/java/com/tiw2019/buyer_seller/controller/TransactionController.java
+++ MicroserviceBank/src/main/java/com/tiw2019/buyer_seller/controller/TransactionController.java
@@ -1,5 +1,8 @@
 package com.tiw2019.buyer_seller.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -38,12 +41,20 @@ public class TransactionController {
 	}
 
 	@RequestMapping(value = "/transactions", method = RequestMethod.POST, produces = "application/json")
-	public ResponseEntity<?> sendTransaction(@RequestBody Transaction transaction) {
+	public ResponseEntity<?> sendTransaction(@RequestBody Transaction transaction) throws ParseException {
 		
+		long cardNumber = Long.parseLong(transaction.getCard_number());
 		int cardNumberLength = transaction.getCard_number().length();
 		int cvvLength = transaction.getCvv().length();
 		
-		if(cardNumberLength == 16 && Long.parseLong(transaction.getCard_number()) % 3 == 0 && cvvLength == 3) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date cardExpiryDate = sdf.parse(transaction.getExpiry_date());
+		Date date = new Date();
+		String currentDate = sdf.format(date);
+		Date current = sdf.parse(currentDate);
+		
+		
+		if(cardNumberLength == 16 &&  cardNumber % 3 == 0 && cardExpiryDate.after(current) && cvvLength == 3) {
 			try {
 				Transaction tran = transactionDAO.save(transaction);
 				
